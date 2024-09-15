2c45da79e0fbd3821421fc8ef0f5e43d5d264b06
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-09-15 15:52:43 +0200

    chore: lint

--- actions/rollup/rollup.config.mjs
+++ actions/rollup/rollup.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
 import { builtinModules } from "node:module";
-import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
 import json from "@rollup/plugin-json";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
 
 /**
  * @type {import("rollup").RollupOptions}
